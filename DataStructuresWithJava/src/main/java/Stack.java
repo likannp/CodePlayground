52b1d6228362d2623af2c7a34ead3bcaba4e8642
Elisangela Silva <elisangela00silva@gmail.com>
2025-02-06 13:40:36 -0300

    aula 03: trabalhando com pilha. Método para adicionar elemento na pilha.

--- DataStructuresWithJava/src/main/java/Stack.java
+++ DataStructuresWithJava/src/main/java/Stack.java
@@ -17,6 +17,18 @@ public class Stack {
     public boolean empty() {
         return topo == -1;
     }
+
+    public boolean push(int valor) {
+        if (!full()) {
+            topo++;
+            pilha[topo] = valor;
+            return true;
+        } else {
+            System.out.println("Pilha cheia! Não é possível adicionar mais elementos.");
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Tamanho da pilha: ");
@@ -25,3 +37,4 @@ public class Stack {
     }
 
 }
+
